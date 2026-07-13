976154b6e77eeeb508d5d17171702eb2589a2e51
AndrewTour <andrew.tour8@gmail.com>
2026-07-13 23:45:23 +1000

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDwa_eMrRRLgQhJwGx3qrRpS-hI3CbctxQ",
+  authDomain: "daily-accountability-be0ac.firebaseapp.com",
+  projectId: "daily-accountability-be0ac",
+  storageBucket: "daily-accountability-be0ac.firebasestorage.app",
+  messagingSenderId: "948051142195",
+  appId: "1:948051142195:web:7dbe6fb935d3fc6b6d6546"
+};
--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,5 @@
-const CACHE_NAME = 'daily-accountability-v6';
-const APP_SHELL = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-    const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response;
-  }).catch(() => caches.match('./index.html'))));
-});
+const CACHE='daily-accountability-v1-0';
+const SHELL=['./','./index.html','./styles.css','./app.js','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
