8c9e9bc86e314c6570a7646d4e5bda94eae9d517
AndrewTour <andrew.tour8@gmail.com>
2026-07-15 17:02:40 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='daily-accountability-v1.23.8-unified-metric-capsule-fix';
+const CACHE='daily-accountability-v1.23.9-appointment-booking-calendar';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
