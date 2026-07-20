9bda6b8588e15a509cb60021142286fb0c2a1b2e
AndrewTour <andrew.tour8@gmail.com>
2026-07-20 23:56:47 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='agnt-v73-1-contact-timeline';
-const ASSETS=['./','./index.html','./styles.css?v=73.1','./app.js?v=73.1','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='agnt-v72-prospecting-intelligence';
+const ASSETS=['./','./index.html','./styles.css?v=72','./app.js?v=72','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
