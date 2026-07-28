d8f595d3957ce2a633a5135f3b4a1381682da15b
AndrewTour <andrew.tour8@gmail.com>
2026-07-28 23:40:31 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='agnt-v111-daily-knocking-totals';
+const CACHE='agnt-v111-daily-knocking-end-fix';
 const ASSETS=['./','./index.html','./styles.css?v=111','./app.js?v=111','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
