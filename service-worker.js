27ed0c1b2c86ce66b4073372678bad9bafc039cc
AndrewTour <andrew.tour8@gmail.com>
2026-07-29 14:03:22 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='agnt-v114-2-smart-log-phase1';
-const ASSETS=['./','./index.html','./styles.css?v=114.2-smart-log','./app.js?v=114.2-smart-log','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='agnt-v114-3-agent-summary';
+const ASSETS=['./','./index.html','./styles.css?v=114.3-agent-summary','./app.js?v=114.3-agent-summary','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
